a0cb3f10fa21f19a2a3c08fe7da0eefce8f86624
Unknown <yannick.bals@web.de>
2017-09-08 11:25:28 +0200

    Small fixes
    Fix evaluate func to not generate mostly negative numbers, make nrs in tree bold

--- src/lib/minMaxHelper.js
+++ src/lib/minMaxHelper.js
@@ -3,7 +3,7 @@
 export const evaluate = () => {
   var random = Math.floor(Math.random() * 10);
   //generates a negative number ~50% of the time
-  var randomizeSign = random * (Math.floor(Math.random() * 2) === 1 ? 1 : -1);
+  var randomizeSign = random * (Math.floor(Math.random() * 2) >= 1 ? 1 : -1);
   return randomizeSign;
 };
 
