a145ec23614e3a8d3ea4fe385287f2a71ffd11db
Unknown <christophcaps@gmail.com>
2017-09-13 00:04:31 +0200

    add react-lineto

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,7 +2,10 @@ import React from 'react';
 import {DepthSetting} from './DepthSetting';
 import {OptionSetting} from './OptionSetting';
 import {connect} from 'react-redux';
-import {setMaxDepth, setMaxOptions} from '../redux'
+import {setMaxDepth, setMaxOptions} from '../redux';
+import LineTo from 'react-lineto';
+import Line from 'react-lineto';
+
 
 const Settings = (props) => {
     return(
@@ -12,6 +15,12 @@ const Settings = (props) => {
             <OptionSetting maxOptions={props.maxOptions} onChange={props.onChangeOptions} />
             <div className="player-label">Starting Player: {props.startingPlayer}</div>
             <button onClick={props.startMinMax}>Start MinMax</button>
+
+            {/*Test react-lineto*/}
+            <div className="A">Element A</div>
+            <div className="B">Element B</div>
+            <LineTo from="A" to="B" border="3px solid black" />
+            <Line x0={0} y0={0} x1={1000} y1={1000} border="5px solid black" />
         </div>
     )
 }
